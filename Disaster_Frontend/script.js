2fa8e8907d625d713be4e4e4fe0e07d4c3744565
anjali-131 <anjalisaraswat415@gmail.com>
2026-03-29 21:31:18 +0530

    updated Frontend

--- Disaster_Frontend/script.js
+++ Disaster_Frontend/script.js
@@ -3,21 +3,33 @@ document.addEventListener("DOMContentLoaded", function(){
 let reportForm = document.getElementById("reportForm");
 
 if(reportForm){
+
 reportForm.addEventListener("submit", function(e){
+
 e.preventDefault();
-alert("✅ Disaster report submitted successfully!");
+
+alert("Disaster report submitted successfully!");
+
 reportForm.reset();
+
 });
+
 }
 
 let helpForm = document.getElementById("helpForm");
 
 if(helpForm){
+
 helpForm.addEventListener("submit", function(e){
+
 e.preventDefault();
-alert("✅ Help request submitted successfully!");
+
+alert("Help request submitted successfully!");
+
 helpForm.reset();
+
 });
+
 }
 
 });
