16a47d6e4886357f8dc0bea1f27605a5c8360a97
anushka981 <anushka.bansal_cs23@gla.ac.in>
2026-08-08 21:58:44 +0530

    merge latest backend updates

--- src/main/java/com/anushka/disaster_backend/DisasterBackendApplication.java
+++ src/main/java/com/anushka/disaster_backend/DisasterBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DisasterBackendApplication {
+public class DisasterBackendApplcation {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DisasterBackendApplication.class, args);
